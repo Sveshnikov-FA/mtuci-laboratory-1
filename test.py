97582017130b582e145c17915ad7d5d4538d3baa
Sveshnikov-FA <strgoose@ya.ru>
2022-02-08 13:16:26 +0300

    feat: вывод сообщения по условию

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
+from turtle import distance
+
+
 HEIGHT = 183
 WEIGHT = 72
 STEPS = 15250
 TIME = 55
 def CaloriesCalc(h,w,s,t):
 	return 0.035 * w + ((s/t)**2 / h) * 0.029 * w
-
 
 def DistanceCalc(h,s):
 	return (((h/4 + 0.37) * s)/100)/1000
@@ -13,5 +15,10 @@ def DistanceCalc(h,s):
 
 # Калорий сожжено: 879.71
 
-print(f"Дистанция в км: {DistanceCalc(HEIGHT,STEPS)}")
+distance = DistanceCalc(HEIGHT,STEPS)
+print(f"Дистанция в км: {distance}")
 # Дистанция в км: 7.03
+
+message = "Дистанция больше 4 км" if distance > 4 else "Дистанция меньше 4 км"
+print(message)
+
