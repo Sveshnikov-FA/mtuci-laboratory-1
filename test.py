3f592be1b0e0403b90c06407fe1b77c46f00cde0
Sveshnikov-FA <36130849+Sveshnikov-FA@users.noreply.github.com>
2022-02-08 12:24:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ TIME = 55
 def L1Calc(h,w,s,t):
 	return 0.035 * w + ((s/t)**2 / h) * 0.029 * w
 
-print(f"Калорий сожжено: {L1Calc(HEIGHT,WEIGHT,STEPS,TIME)}\nПройденная дистанция: {STEPS/TIME}")
+print(f"Калорий сожжено: {L1Calc(HEIGHT,WEIGHT,STEPS,TIME)}; Пройденная дистанция: {STEPS/TIME}")
 
-# Калорий сожжено: 879.71
-# Пройденная дистанция: 277.27
+# Калорий сожжено: 879.71; Пройденная дистанция: 277.27
