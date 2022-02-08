fd72035478a9043f99f782e81000d1ddc76882b4
Sveshnikov-FA <strgoose@ya.ru>
2022-02-08 13:22:33 +0300

    fix: naming

--- test.py
+++ test.py
@@ -1,25 +1,28 @@
-from turtle import distance
-
-
 HEIGHT = 183
 WEIGHT = 72
 STEPS = 15250
 TIME = 55
-def CaloriesCalc(h,w,s,t):
+
+def calories_calc(h,w,s,t):
 	return 0.035 * w + ((s/t)**2 / h) * 0.029 * w
 
-def DistanceCalc(h,s):
+def distance_calc(h,s):
 	return (((h/4 + 0.37) * s)/100)/1000
 
-print(f"Калорий сожжено: {CaloriesCalc(HEIGHT,WEIGHT,STEPS,TIME)}")
-
+calories = calories_calc(HEIGHT,WEIGHT,STEPS,TIME)
+print(f"Калорий сожжено: {calories}")
 # Калорий сожжено: 879.71
 
-distance = DistanceCalc(HEIGHT,STEPS)
+distance = distance_calc(HEIGHT,STEPS)
 print(f"Дистанция в км: {distance}")
 # Дистанция в км: 7.03
 
-message = "Дистанция больше 4 км" if distance > 4 else "Дистанция меньше 4 км"
-print(message)
+
+if distance > 4:
+	print("Дистанция больше 4 км")
+elif distance > 2:
+	print("Дистанция больше 2 км")
+else:
+	print("Дистанция меньше 2 км")
 # Дистанция больше 4 км
 
