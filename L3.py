589fc4d0028b82563ecde2cc189aaa57a3847fcc
Sveshnikov-FA <strgoose@ya.ru>
2022-02-19 12:34:16 +0300

    fix: added docstrings

--- L3.py
+++ L3.py
@@ -4,15 +4,30 @@ import logging
 
 
 class Calculator:
-
+    """
+        Калькулятор для подсчёта записей.
+        :parameter
+        limit: Целое число, лимит по операциям
+        records: Массив целых чисел с операциями
+    """
     def __init__(self, limit):
         self.limit = limit
         self.records = []
 
     def add_record(self, record):
+        """
+            Добавляет операцию в массив
+            :parameter
+            records: объект класса Record
+        """
         self.records.append(record)
 
     def get_today_stats(self):
+        """
+            Подсчёт операций за сегодняшний день
+            :return:
+            total: Целое число, сумма всех операций
+        """
         total = 0
         for item in self.records:
             if datetime.date.today() == item.date:
@@ -23,6 +38,11 @@ class Calculator:
         return total
 
     def get_week_stats(self):
+        """
+            Подсчёт операций за предыдущие 7 дней
+            :return:
+            total: Целое число, сумма всех операций
+        """
         total = 0
         for wd in range(7):
             for item in self.records:
@@ -34,11 +54,16 @@ class Calculator:
 
 
 class CaloriesCalculator(Calculator):
-
+    """
+        Калькулятор для подсчёта калорий. Наследует методы и параметры класса Calculator
+    """
     def __init__(self, limit):
         super().__init__(limit)
 
     def get_calories_remained(self):
+        """
+            Определяет, превышен ли лимит калорий на сегодня
+        """
         received = self.get_today_stats()
         if received < self.limit:
             logging.warning(f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit - received} кКал")
@@ -47,11 +72,20 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
-
+    """
+        Калькулятор для подсчёта денег. Наследует методы и параметры класса Calculator
+    """
     def __init__(self, limit):
         super().__init__(limit)
 
     def get_today_cash_remained(self, currency):
+        """
+            Определяет, сколько денег осталось на счету за сегодня по указанному курсу
+            :parameter
+            currency: строка, обозначающая тип валюты
+            :return
+            result: действительное число, остаток на счету в указанной валюте
+        """
         curr_mapping = requests.get("http://api.exchangeratesapi.io/v1/latest?access_key=bf6cf289b53ad57e768c3cabc9630636").json()["rates"]
         for i in curr_mapping:
             curr_mapping[i] = round(curr_mapping[i]/curr_mapping["RUB"], 2)
@@ -63,7 +97,13 @@ class CashCalculator(Calculator):
 
 
 class Record:
-
+    """
+        Объект произведённой операции
+        :parameter
+        amount: целое число, количество задействованных у.е. в операции
+        date: datetime.date, дата произведения операции
+        comment: строка, комментарий к операции
+    """
     def __init__(self, amount=0, date=datetime.date.today(), comment=""):
         self.amount = amount
         self.date = datetime.datetime.strptime("%Y-%m-%d").date() if type(date) is str else date
